8ce885788bbb9077df9188bb84034d33e93bf9f8
piet <you@example.com>
2026-01-19 15:16:26 +0100

    linted

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,28 +1,39 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function Alert({ type='information', heading, children, closable, onClose }) {
-      const [visible, setVisible] = useState(true)
-      function handleCloseClick() {
-        setVisible(false)
-        if (onClose) {
-            onClose()
-        }
-      }
-  if (!visible) 
-        return null
-    return (
-        <div>
-            <div>
-                <span role="img" aria-label={type === 'warning' ? 'Warning': 'Information'}>
-                    {type === 'warning' ? 'Warning': 'Information'}
-                </span>
-                <span>{heading}</span>
-            </div>
-            {closable &&
-            <button aria-label="Close" onClick={handleCloseClick}>
-                <span role="img" aria-lable="Close">x</span>
-            </button>}
-            <div>{children}</div>
-        </div>
-    )
+export function Alert({
+  type = "information",
+  heading,
+  children,
+  closable,
+  onClose,
+}) {
+  const [visible, setVisible] = useState(true);
+  function handleCloseClick() {
+    setVisible(false);
+    if (onClose) {
+      onClose();
+    }
+  }
+  if (!visible) return null;
+  return (
+    <div>
+      <div>
+        <span
+          role="img"
+          aria-label={type === "warning" ? "Warning" : "Information"}
+        >
+          {type === "warning" ? "Warning" : "Information"}
+        </span>
+        <span>{heading}</span>
+      </div>
+      {closable && (
+        <button aria-label="Close" onClick={handleCloseClick}>
+          <span role="img" aria-lable="Close">
+            x
+          </span>
+        </button>
+      )}
+      <div>{children}</div>
+    </div>
+  );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { Alert } from './Alert'
-
-
+import { Alert } from "./Alert"
 
 function App() {
-
-  return (<Alert heading="Success" closable onClose={()=> console.log("closed defined in App")}>Everthing is really good!</Alert>)
+  return (
+    <Alert
+      heading="Success"
+      closable
+      onClose={() => console.log("closed defined in App")}
+    >
+      Everything is really good!
+    </Alert>
+  )
 }
 
 export default App
